aed952a0228927d84b3e5bd67b619f0b13dd24b2
Alexis Lopez <lopezalexis499@gmail.com>
2022-11-21 14:02:44 -0300

    agregando javascript

---
+++ index.js
@@ -0,0 +1,44 @@
+// let numero1 = 1;
+// let numero2 = 15;
+// console.log(numero1 + numero2);
+
+// let mensaje = "hola "
+// let nombre = "alexis Lopez"
+// console.log(mensaje + nombre);
+
+// let numero1 = parseInt(prompt("ingrese su primer numeor "));
+// let numero2 = parseInt(prompt("ingrese su segundo numero "));
+
+// alert("el resultado de la suma es " + (numero1 + numero2));
+
+// clase 2
+
+// estructura de un condicional
+
+// let numero1 = parseInt(prompt("escribe tu numero"));
+
+// if(numero1 > 1000 && numero1 < 3000) {
+//     alert("tu numero es correcto")
+// }else{
+//     alert("tu numero es muy bajito o demasiado alto")
+// }
+
+// let saludo = prompt("escribe tu saludo").toUpperCase()
+
+// if(saludo === "HOLA"){
+//     alert("hola como estas ?")
+// }else{
+//     alert("saluda hdp")
+// }
+
+let nombre = prompt("escribe tu nombre").toLowerCase()
+
+if(nombre == "brisa"){
+    alert("hola brisa, suerte en los finales")
+}else if(nombre == "jesy" || nombre == "jesica"){
+    alert("anda a cuidar el gatito")
+}else if(nombre == "lety" || nombre == "leticia"){
+    alert("avisa cuando llegues de daniel")
+}else if(nombre == "naiara" || nombre == "princesa" || nombre == "mirna"){
+    alert("te amo!")
+}
